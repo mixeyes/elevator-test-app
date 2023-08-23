bce69cb2c3e7952da05759b3c7661bcb0584cf1e
mixeyes <lelyakov_mikhail@nixsolutions.com>
2023-08-23 17:00:19 +0200

    create app folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React, { Suspense, lazy } from 'react';
-import { GlobalStyle } from './styles';
-import { Layout } from './components';
+import { GlobalStyle } from '../styles';
+import { Layout } from '../components';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-const ConfigPage = lazy(() => import('./pages/ConfigPage'));
-const ElevatorsPage = lazy( () => import('./pages/ElevatorsPage'));
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const ConfigPage = lazy(() => import('../pages/ConfigPage'));
+const ElevatorsPage = lazy(() => import('../pages/ElevatorsPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
 function App() {
   return (
